5702d55e885ea2cc8876a57ba32a530659c456a5
bao1405 <147390596+bao1405@users.noreply.github.com>
2024-01-16 08:16:30 +0700

    Update bai1.js

--- bai1.js
+++ bai1.js
@@ -1,4 +1,13 @@
-let a = +prompt("Nhap 1 so nguyen duong");
-for(let i=1;i<=a;i++){
-    console.log(i);
+let a = +prompt("Nhập a");
+let sum = 0;
+
+if (!isNaN(a)) {
+    for (let i = 1; i <= a; i++) {
+        if ((a % 4 === 0 && i <= a - 2) || (a % 4 === 3 && i <= a - 1) || (a % 4 !== 0 && a % 4 !== 3)) {
+            sum += i;
+        }
+    }
+    console.log('Tổng: ' + sum);
+} else {
+    console.log("Không hợp lệ");
 }
