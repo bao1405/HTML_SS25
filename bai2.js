81d58be1df62bc186328dd84ccca4fd09649628f
bao1405 <147390596+bao1405@users.noreply.github.com>
2024-01-16 08:17:19 +0700

    Update bai2.js

--- bai2.js
+++ bai2.js
@@ -1,6 +1,17 @@
-let a = +prompt("Nhap 1 so");
-let kq=1;
-for(let i=1;i<=a;i++){
-    kq = kq*i;
+let a = +prompt("Nhập số nguyên dương a:");
+let b = +prompt("Nhập số nguyên dương b:");
+if (a > b) {
+    [a, b] = [b, a];
 }
-console.log(kq);
+
+let numle = 0;
+let numchan = 0;
+for (let i = a + 1; i < b; i++) {
+    if (i % 2 !== 0) {
+        numle += i;
+    } else {
+        numchan += i;
+    }
+}
+console.log(`Tổng số lẻ từ ${a} đến ${b}: ${numle}`);
+console.log(`Tổng số chẵn từ ${a} đến ${b}: ${numchan}`);
